9730419d648c64ca54e8edc243f86ba55324a65d
Nicholas <nicholasss@users.noreply.github.com>
2026-03-02 16:49:51 -0500

    first attempt at the note type and note interfaces

---
+++ internal/models/note.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Note represents a single note.
+type Note struct {
+	ID            uuid.UUID
+	Author        uuid.UUID
+	NoteCreatedAt time.Time
+	NoteUpdatedAt time.Time
+	NoteText      []byte
+}
+
+// NoteService is implemented by the note service within internal/services.
+type NoteService interface {
+	// Creates and returns a new note object after storing it in the repository.
+	CreateNote(ctx context.Context, author uuid.UUID, createdAt time.Time, text []byte) (*Note, error)
+
+	// Retrieves a stored note from the repository.
+	GetNote(ctx context.Context, noteID uuid.UUID) (*Note, error)
+
+	// Retrieves all notes stored in the repository.
+	GetAllNotes(ctx context.Context) (*[]Note, error)
+
+	// Updates specified note in the repository and returns the updated note.
+	UpdateNote(ctx context.Context, noteID uuid.UUID, updatedAt time.Time, newText []byte) (*Note, error)
+	
+	// Deletes the specified note from within the repository.
+	DeleteNote(ctx context.Context, noteID uuid.UUID) error
+}
+
+// NoteRepository is implemented by the note repository within internal/repository with a concrete database.
+type NoteRepository interface {
+	// Creates a new note in the repository.
+	InsertNote(ctx context.Context, note *Note) (*Note, error)
+
+	// Retrieves note from the repository.
+	QueryNote(ctx context.Context, noteID uuid.UUID) (*Note, error)
+
+	// Retrieves all notes from the repository.
+	QueryAllNotes(ctx context.Context) (*[]Note, error)
+
+	// Updates the provided note, using its ID, within the repository.
+	AlterNote(ctx context.Context, note *Note) (*Note, error)
+
+	// Deletes the specified note from within the repository.
+	DeleteNote(ctx context.Context, note *Note) error
+}
