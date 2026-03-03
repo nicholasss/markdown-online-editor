47d1f4e505ea56ad60c29369fd082c36f117f15a
Nicholas <nicholasss@users.noreply.github.com>
2026-03-03 13:33:20 -0500

    wrote first method for repository

--- internal/sqlite_repository/sqlite.go
+++ internal/sqlite_repository/sqlite.go
@@ -1,10 +1,16 @@
 package sqlite
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"fmt"
+	"log"
+	"time"
 
+	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
+	note "github.com/nicholasss/markdown-online-editor/internal/models"
 )
 
 const DBDriver = "sqlite3"
@@ -12,7 +18,7 @@ const DBDriver = "sqlite3"
 func NewSqliteRepository(DBConnectionString string) (*SqliteRepository, error) {
 	db, err := sql.Open(DBDriver, DBConnectionString)
 	if err != nil {
-		fmt.Printf("ERROR: %s\n", err)
+		log.Printf("ERROR: %s\n", err)
 		return nil, err
 	}
 
@@ -23,6 +29,66 @@ type SqliteRepository struct {
 	DB *sql.DB
 }
 
-func (repo *SqliteRepository) CloseRepository() error {
-	return repo.DB.Close()
+func (r *SqliteRepository) CloseRepository() error {
+	return r.DB.Close()
+}
+
+func (r *SqliteRepository) InsertNote(ctx context.Context, newNote *note.Note) (*note.Note, error) {
+	// Check for a nil pointer being passed in
+	if newNote == nil {
+		return nil, errors.New("nil pointer provided")
+	}
+
+	// Query literal
+	query := `INSERT INTO expenses (
+  	id,
+  	created_at,
+  	updated_at,
+  	note_text
+	) values (
+		?,
+		unixepoch(),
+		unixepoch(),
+		?
+	) RETURNING *;`
+
+	// Construct the row query
+	row := r.DB.QueryRowContext(ctx, query, newNote.ID, newNote.NoteText)
+
+	// Execute query with scan
+	var queryID uuid.UUID
+	var queryCreatedAt int64
+	var queryUpdatedAt int64
+	var queryNoteText []byte
+	err := row.Scan(&queryID, &queryCreatedAt, &queryUpdatedAt, &queryNoteText)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("unable to insert row %w", err)
+	} else if err != nil {
+		return nil, err
+	}
+
+	// Construct returning object
+	queryNote := note.Note{
+		ID:            queryID,
+		NoteCreatedAt: time.Unix(queryCreatedAt, 0),
+		NoteUpdatedAt: time.Unix(queryUpdatedAt, 0),
+		NoteText:      queryNoteText,
+	}
+	return &queryNote, nil
+}
+
+func (r *SqliteRepository) QueryNote(ctx context.Context, noteID uuid.UUID) (*note.Note, error) {
+	return nil, nil
+}
+
+func (r *SqliteRepository) QueryAllNotes(ctx context.Context) (*[]note.Note, error) {
+	return nil, nil
+}
+
+func (r *SqliteRepository) AlterNote(ctx context.Context, note *note.Note) (*note.Note, error) {
+	return nil, nil
+}
+
+func (r *SqliteRepository) DeleteNote(ctx context.Context, note *note.Note) error {
+	return nil
 }
